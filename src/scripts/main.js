0ffe8daa1693fd1f418b99578027c4f981bb1b15
Mat Ruszczycky <mat_ruszczycky@apple.com>
2022-12-14 14:53:15 -0800

    dev: checkpoint

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,6 +9,27 @@ import * as NoJs from './modules/noJS';
 // Main
 const Main = () => {
     NoJs.init();
+
+    const videos = document.querySelectorAll("[data-video-id]");
+
+    for (let i = 0, len = videos.length; i < len; i++) {
+        videos[i].addEventListener('click', function(e) {
+            activeModal = document.querySelector(`#${this.dataset.videoId}`).classList.add('modal-open');
+        });
+    }
+
+
+    const modalClose = document.querySelectorAll(".modal-close");
+    
+
+    for (let j = 0, len = modalClose.length; j < len; j++) {
+        modalClose[j].addEventListener('click', function(e) {
+            const modal = document.querySelectorAll(".modal");
+            for (let k = 0, lenk = modalClose.length; k < lenk; k++) {
+                modal.classList.remove('modal-open');
+            }
+        });
+    }
 };
 
 document.addEventListener('readystatechange', e => {
